bc0abab710c2d369fcbcc26d34fbcb90fed096e1
VastiDev <vastiane1@gmail.com>
2023-09-02 15:47:25 -0300

    renomeia properties

---
+++ src/main/java/dev/wakanadaacademy/desafio1pessoas/Desafio1PessoasApplication.java
@@ -0,0 +1,13 @@
+package dev.wakanadaacademy.desafio1pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio1PessoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Desafio1PessoasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/wakanadaacademy/desafio1pessoas/Desafio1PessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakanadaacademy.desafio1pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio1PessoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
